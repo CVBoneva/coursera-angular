c475945eee6f61ed685ce15975ae9cb3f0f859e6
CVBoneva <cveta.boneva@gmail.com>
2024-03-06 13:32:08 +0100

    solution  maart

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -3,25 +3,92 @@
   {
     'use strict';
 
-    angular.module('MsgApp', [])
+    angular.module('LunchCheck', [])
     
-    .controller('MsgController', MsgController);
+    .controller('LunchCheckController', LunchCheckController);
 
-    MsgController.$inject = ['$scope'];
-    function MsgController($scope)
+    LunchCheckController.$inject = ['$scope'];
+    function LunchCheckController($scope)
     {
-      $scope.name = "BArakaka";
-      $scope.whichImage = "1"
 
-      $scope.sayMessage = function()
+      const strEmptyField = "Please enter data first";
+      const strEnjoy = "Enjoy!";
+      const strTooMuch = "Too much!";
+      const strNotValid = "Please enter valid data";
+      const fieldColors = {msgOk: "greenValidation", msgNotOk: "redValidation", msgDefault: "defaultValidation"};
+      $scope.classColors = fieldColors.msgDefault;
+
+      var initString = function()
       {
-        return $scope.name + ", you should say something no matter what";
+        return "";
       }
 
-      $scope.showNextImage = function()
+      $scope.clearMessages = function()
       {
-        $scope.whichImage = $scope.whichImage == "1" ? "2" : "1"
+        $scope.errorMsg = initString();
+        $scope.resultMsg = initString();
+        $scope.classColors = fieldColors.msgDefault;
       }
+
+      $scope.lunchItems = initString();
+      $scope.clearMessages();
+
+      $scope.checkIfTooMuch = function()
+      {
+        
+        $scope.clearMessages();
+
+        if (!$scope.checkInput ())
+        {
+          return true;
+        }
+
+        $scope.resultMsg = $scope.countItems();
+        return true;
+      }
+
+      $scope.checkInput = function()
+      {
+        if ($scope.lunchItems == "")
+        {
+          $scope.errorMsg = strEmptyField;
+          $scope.classColors = fieldColors.msgNotOk;
+          return false;
+        }
+        return true;
+      }
+
+      $scope.countItems = function()
+      {
+        var str = $scope.lunchItems;
+        var arr = str.split(',');
+
+        function isEmpty(value) { 
+          console.log(value);
+          return !(value == "" || value == null || value == undefined); 
+        } 
+
+        var arr1 = arr.filter(isEmpty);
+
+        
+        if (arr1.length == 0)
+        {
+          $scope.errorMsg = strNotValid;
+          $scope.classColors = fieldColors.msgNotOk;
+          return '';
+        }
+        else if (arr1.length <= 3)
+        {
+          $scope.classColors = fieldColors.msgOk;
+          return strEnjoy;
+        }
+        else
+        {
+          $scope.classColors = fieldColors.msgNotOk;
+          return strTooMuch;
+        }
+      }
+     
     }
         
   }
